24982602c53d10b32e6bdc0560b21eca7c19bbfa
Marijsoft <marijsoftdevteam@gmail.com>
2025-12-25 20:54:28 +0100

    Add files via upload

--- LibreDelphi.pas
+++ LibreDelphi.pas
@@ -2,6 +2,7 @@
 { Delphi component library for LibreTranslator service   }
 { relased under license AGPL 3.0                         }
 { Copyright (C) 2022 Created by MarijSoft. 28/09/2022    }
+{ Last update:25/12/2025                                 }
 { ****************************************************** }
 { Platform supported:Win,Linux,MacOS,Android,IOS         }
 { ****************************************************** }
@@ -13,287 +14,274 @@ interface
 uses
   System.Net.URLClient, System.Net.HttpClient, System.Net.HttpClientComponent,
   System.Net.Mime, System.JSON, System.Types, System.UITypes, System.Classes,
-  System.SysUtils,
-  System.Variants, System.Generics.Collections, System.Messaging;
+  System.SysUtils, System.Variants, System.Generics.Collections,
+  System.Messaging;
 
 type
+  TLibreEndpointSite = (
+    lesLibreTranslateMain,      // libretranslate.com
+    lesLibreTranslateDE,        // libretranslate.de
+    lesArgosOpenTech,           // translate.argosopentech.com
+    lesVernCC,                  // lt.vern.cc
+    lesFortyTwoIT,              // translate.fortytwo-it.com
+    lesTerraPrint,              // translate.terraprint.co
+    lesSkitzen,                 // translate.api.skitzen.com
+    lesZillyHuhn,               // trans.zillyhuhn.com
+    lesFedilab                  // ok no api key
+  );
 
-  [ComponentPlatformsAttribute($000B945F)]
+[ComponentPlatforms(
+  pidWin32 or pidWin64 or
+  pidOSX32 or pidOSX64 or
+  pidLinux64 or
+  pidAndroid or
+  pidIOSSimulator or pidIOSDevice
+)]
   TLibreTrans = class(TComponent)
   private
-    resturl: string;
-    function sitonline(sitoweb: string): boolean;
+    FAPI: string;
+    FSite: TLibreEndpointSite;
+    FEndpoint: string;
+    FNet: TNetHTTPClient;
+    procedure SetSite(const Value: TLibreEndpointSite);
+    function SitOnline(const URL: string): Boolean;
   protected
-    apikey: string;
-    function keyapi: string;
-    function urlendpoint(index: integer): string;
+    function DefaultApiKey: string;
   public
     constructor Create(AOwner: TComponent); override;
     destructor Destroy; override;
+    function CheckLng(const Text: string): string;
+    function ListLng: TStringList;
+    function CodLng: TStringList;
+    function Translate(const Text, OrgLng, DstLng: string): string;
+    function AutoTranslate(const Text, DstLng: string): string;
+    function TranslateFile(const Orig, Dest, LngOrig, LngDest: string): string;
+    procedure AutoEndpoint;
+    property Endpoint: string read FEndpoint;
   published
-    property API: string read apikey write apikey;
-    property Endpoint: string read resturl write resturl;
-    function checklng(Text: string): string;
-    function listlng: TStringList;
-    function codlng: TStringList;
-    function translatefile(orig, dest, lngorig, lngdest: string): string;
-    function translate(Text, orglng, dstlng: string): string;
-    function autotranslate(Text, dstlng: string): string;
-    procedure autoendpoint;
+    property API: string read FAPI write FAPI;
+    property Site: TLibreEndpointSite read FSite write SetSite default lesLibreTranslateMain;
   end;
 
-const
-  ty: TArray<String> = ['text/plain', 'application/vnd.oasis.opendocument.text',
-    'application/vnd.oasis.opendocument.presentation',
-    'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-    'application/vnd.openxmlformats-officedocument.presentationml.presentation',
-    'application/epub+zip', 'type=text/html'];
-
-const
-sito:
-TArray < String >= ['https://lt.vern.cc/',
-  'https://translate.argosopentech.com/', 'https://libretranslate.com/',
-  'https://translate.fortytwo-it.com/', 'https://translate.terraprint.co/',
-  'https://libretranslate.de/'];
-
-var net:tnethttpclient;
-
-Procedure Register;
+procedure Register;
 
 implementation
 
-{ TLibreTrans }
+const
+  CEndpointUrls: array[TLibreEndpointSite] of string = (
+    'https://libretranslate.com/',
+    'https://libretranslate.de/',
+    'https://translate.argosopentech.com/',
+    'https://lt.vern.cc/',
+    'https://translate.fortytwo-it.com/',
+    'https://translate.terraprint.co/',
+    'https://translate.api.skitzen.com/',
+    'https://trans.zillyhuhn.com/',
+    'https://translate.fedilab.app/'
+  );
 
-Procedure Register;
+ty: TArray<String> = ['text/plain', 'application/vnd.oasis.opendocument.text',
+'application/vnd.oasis.opendocument.presentation',
+'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+'application/epub+zip', 'type=text/html'];
+
+procedure Register;
 begin
   RegisterComponents('MarijSoft', [TLibreTrans]);
 end;
 
-function TLibreTrans.codlng: TStringList;
-var
-  lng: TJsonValue;
-  risp, ris: string;
-  x: integer;
-  JAr: TJSONArray;
-  Jobj: TJSONObject;
-  begin
-  risp := Net.Get(Endpoint + '/languages').ContentAsString();
-  lng := TJSONObject.ParseJSONValue(risp) as TJsonValue;
-  Result := TStringList.Create;
-  try
-    JAr := lng as TJSONArray;
-    for x := 0 to JAr.count - 1 do
-    begin
-      Jobj := JAr[x] as TJSONObject;
-      Result.Add(Jobj.GetValue('code').Value);
-    end;
-  finally
-    lng.DisposeOf;
-  end;
-end;
-
-procedure TLibreTrans.autoendpoint;
-begin
-  for var I: integer := Low(sito) to High(sito) do
-  begin
-    if sitonline(sito[I]) then
-    begin
-      urlendpoint(I);
-      break
-    end;
-  end;
-end;
+{ TLibreTrans }
 
 constructor TLibreTrans.Create(AOwner: TComponent);
-var net:thethttpclient;
 begin
-  inherited Create(AOwner);
-  apikey := keyapi;
-  Net := TNetHttpClient.Create(nil);
+  inherited;
+  FNet := TNetHTTPClient.Create(nil);
+  FAPI := DefaultApiKey;
+  Site := lesLibreTranslateMain; // default design-time
 end;
 
 destructor TLibreTrans.Destroy;
 begin
+  FreeAndNil(FNet);
   inherited;
-  freeandnil(net);
 end;
 
-function TLibreTrans.keyapi: string;
+function TLibreTrans.DefaultApiKey: string;
 begin
   Result := 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx';
 end;
 
-function TLibreTrans.listlng: TStringList;
-var
-  lng: TJsonValue;
-  risp: string;
-  x: integer;
-  JAr: TJSONArray;
-  Jobj: TJSONObject;
- begin
-  risp := Net.Get(Endpoint + 'languages').ContentAsString();
-  lng := TJSONObject.ParseJSONValue(risp) as TJsonValue;
-  Result := TStringList.Create;
-  try
-    JAr := lng as TJSONArray;
-    for x := 0 to JAr.count - 1 do
-    begin
-      Jobj := JAr[x] as TJSONObject;
-      Result.Add(Jobj.GetValue('name').Value);
-    end;
-  finally
-    lng.DisposeOf;
-  end;
-end;
-
-function TLibreTrans.sitonline(sitoweb: string): boolean;
-var
-  s: integer;
+procedure TLibreTrans.SetSite(const Value: TLibreEndpointSite);
 begin
-  s := Net.Get(sitoweb, nil).StatusCode;
-  if s = 200 then
-    Result := true
-  else
-    Result := false;
-end;
-
-function TLibreTrans.translate(Text, orglng, dstlng: string): string;
-var
-  prm: TStringList;
-  trad: string;
-  val: TJsonValue;
-begin
-  prm := TStringList.Create;
-  prm.AddPair('q', Text);
-  prm.AddPair('source', orglng);
-  prm.AddPair('target', dstlng);
-  prm.AddPair('format', 'text');
-  prm.AddPair('api_key', apikey);
-  try
-    with Net.Create(nil) do
-      try
-        trad := Net.Post(Endpoint + 'translate', prm)
-          .ContentAsString(TEncoding.UTF8);
-        val := TJSONObject.ParseJSONValue(trad) as TJsonValue;
-        Result := val.GetValue<String>('translatedText');
-      except
-        on E: Exception do
-          raise Exception.Create
-            ('Error Translation check your internet connection' + #13 +
-            'or try again later');
-      end;
-  finally
-    prm.DisposeOf;
-  end;
-end;
-
-function TLibreTrans.translatefile(orig, dest, lngorig,
-  lngdest: string): string;
-var
-  prm: TMultipartFormData;
-  ris, dwn, tipo: String;
-  url: TJsonValue;
-  tm: TMemoryStream;
-  function tipofile(tfile: string): string;
-  var
-    ext: string;
+  if FSite <> Value then
   begin
-    ext := LowerCase(ExtractFileExt(tfile));
-    for var I: integer := Low(ty) to High(ty) do
+    FSite := Value;
+    FEndpoint := CEndpointUrls[FSite];
+  end;
+end;
+
+function TLibreTrans.SitOnline(const URL: string): Boolean;
+begin
+  Result := False;
+  try
+    Result := FNet.Get(URL).StatusCode = 200;
+  except
+    Result := False;
+  end;
+end;
+
+procedure TLibreTrans.AutoEndpoint;
+begin
+  for var S := Low(TLibreEndpointSite) to High(TLibreEndpointSite) do
+    if SitOnline(CEndpointUrls[S]) then
     begin
-      if Pos(ty[I], ext) > 0 then
-      begin
-        Result := ty[I];
-        break;
-      end;
+      Site := S;
+      Break;
     end;
-  end;
-
-begin
-  prm := TMultipartFormData.Create(true);
-  tipo := tipofile(orig);
-  prm.AddFile('file', orig);
-  prm.AddField('type', tipo);
-  prm.AddField('source', lngorig);
-  prm.AddField('target', lngdest);
-  prm.AddField('api_key', apikey);
-  try
-    with Net.Create(nil) do
-      try
-        ris := Net.Post(Endpoint + 'translate_file', prm).ContentAsString();
-        url := TJSONObject.ParseJSONValue(ris) as TJSONObject;
-        dwn := url.GetValue<String>('translatedFileUrl');
-        tm := TMemoryStream.Create;
-        Net.Get(dwn, tm).ContentStream;
-      except
-        on E: Exception do
-          raise Exception.Create
-            ('Error Translate file check your internet connection' + #13 +
-            'or try again later');
-      end;
-  finally
-    tm.Position := 0;
-    tm.SaveToFile(dest);
-    FreeAndNil(tm);
-  end;
 end;
 
-function TLibreTrans.urlendpoint(index: integer): string;
-begin
-  Result := sito[index];
-  Endpoint := Result;
-end;
-
-function TLibreTrans.autotranslate(Text, dstlng: string): string;
+function TLibreTrans.ListLng: TStringList;
 var
-  prm: TStringList;
-  trad: string;
-  val: TJsonValue;
+  JSON: TJSONArray;
 begin
-  prm := TStringList.Create;
-  prm.AddPair('q', Text);
-  prm.AddPair('source', 'auto');
-  prm.AddPair('target', dstlng);
-  prm.AddPair('format', 'text');
-  prm.AddPair('api_key', apikey);
+  Result := TStringList.Create;
+  JSON := TJSONObject.ParseJSONValue(
+    FNet.Get(FEndpoint + 'languages').ContentAsString
+  ) as TJSONArray;
   try
-    with Net.Create(nil) do
-      try
-        trad := Net.Post(Endpoint + 'translate', prm)
-          .ContentAsString(TEncoding.UTF8);
-        val := TJSONObject.ParseJSONValue(trad) as TJsonValue;
-        Result := val.GetValue<String>('translatedText');
-      except
-        on E: Exception do
-          raise Exception.Create
-            ('Error Translation check your internet connection' + #13 +
-            'or try again later');
-      end;
+    for var Item in JSON do
+      Result.Add(Item.GetValue<string>('name'));
   finally
-    prm.DisposeOf;
+    JSON.Free;
   end;
 end;
 
-function TLibreTrans.checklng(Text: string): string;
+function TLibreTrans.CodLng: TStringList;
 var
-  risp: string;
-  param: TStringList;
-  lng: TJsonValue;
+  JSON: TJSONArray;
 begin
-  param := TStringList.Create;
-  param.AddPair('q', Text);
-  param.AddPair('api_key', apikey);
+  Result := TStringList.Create;
+  JSON := TJSONObject.ParseJSONValue(
+    FNet.Get(FEndpoint + 'languages').ContentAsString
+  ) as TJSONArray;
   try
-    risp := Net.Post(Endpoint + 'detect', param).ContentAsString;
-    risp := Trim(risp);
-    risp := risp.Replace('[', '');
-    risp := risp.Replace(']', '');
-    lng := TJSONObject.ParseJSONValue(risp) as TJSONObject;
-    Result := lng.GetValue<String>('language');
+    for var Item in JSON do
+      Result.Add(Item.GetValue<string>('code'));
   finally
-    param.DisposeOf;
+    JSON.Free;
+  end;
+end;
+
+function TLibreTrans.Translate(const Text, OrgLng, DstLng: string): string;
+var
+  Params: TStringList;
+  JSON: TJSONObject;
+begin
+  Params := TStringList.Create;
+  try
+    Params.AddPair('q', Text);
+    Params.AddPair('source', OrgLng);
+    Params.AddPair('target', DstLng);
+    Params.AddPair('format', 'text');
+    Params.AddPair('api_key', FAPI);
+
+    JSON := TJSONObject.ParseJSONValue(
+      FNet.Post(FEndpoint + 'translate', Params)
+        .ContentAsString(TEncoding.UTF8)
+    ) as TJSONObject;
+    try
+      Result := JSON.GetValue<string>('translatedText');
+    finally
+      JSON.Free;
+    end;
+  finally
+    Params.Free;
+  end;
+end;
+
+function TLibreTrans.AutoTranslate(const Text, DstLng: string): string;
+begin
+  Result := Translate(Text, 'auto', DstLng);
+end;
+
+function TLibreTrans.CheckLng(const Text: string): string;
+var
+  Params: TStringList;
+  JSON: TJSONObject;
+begin
+  Params := TStringList.Create;
+  try
+    Params.AddPair('q', Text);
+    Params.AddPair('api_key', FAPI);
+
+    JSON := TJSONObject.ParseJSONValue(
+      FNet.Post(FEndpoint + 'detect', Params).ContentAsString
+    ) as TJSONObject;
+    try
+      Result := JSON.GetValue<string>('language');
+    finally
+      JSON.Free;
+    end;
+  finally
+    Params.Free;
+  end;
+end;
+
+function tipofile(tfile: string): string;
+begin
+  if tfile.Contains('.txt') then
+    Result := ty[0]
+  else if tfile.Contains('.odt') then
+    Result := ty[1]
+  else if tfile.Contains('.odp') then
+    Result := ty[2]
+  else if tfile.Contains('.docx') then
+    Result := ty[3]
+  else if tfile.Contains('.pptx') then
+    Result := ty[4]
+  else if tfile.Contains('.epub') then
+    Result := ty[5]
+  else if tfile.Contains('.html') then
+    Result := ty[6]
+  else
+    raise Exception.Create('Tipo di file non supportato');
+end;
+
+
+function TLibreTrans.TranslateFile(
+  const Orig, Dest, LngOrig, LngDest: string): string;
+var
+  Form: TMultipartFormData;
+  JSON: TJSONObject;
+  MS: TMemoryStream;
+  URL,tipo: string;
+begin
+  Form := TMultipartFormData.Create(True);
+  MS := TMemoryStream.Create;
+  tipo:=tipofile(orig);
+  try
+    Form.AddFile('file', Orig);
+    Form.AddField('type',tipo);
+    Form.AddField('source', LngOrig);
+    Form.AddField('target', LngDest);
+    Form.AddField('api_key', FAPI);
+
+    JSON := TJSONObject.ParseJSONValue(
+      FNet.Post(FEndpoint + 'translate_file', Form).ContentAsString
+    ) as TJSONObject;
+    try
+      URL := JSON.GetValue<string>('translatedFileUrl');
+      FNet.Get(URL, MS);
+      MS.SaveToFile(Dest);
+      Result := Dest;
+    finally
+      JSON.Free;
+    end;
+  finally
+    Form.Free;
+    MS.Free;
   end;
 end;
 
 end.
-
