f590d5595f55f9461299d4bdc5e9c32a7ed487c1
Marijsoft <marijsoftdevteam@gmail.com>
2025-12-26 03:17:53 +0100

    Add files via upload

---
+++ Win64x/Release/LibreDelphi.hpp
@@ -0,0 +1,85 @@
+﻿// CodeGear C++Builder
+// Copyright (c) 1995, 2025 by Embarcadero Technologies, Inc.
+// All rights reserved
+
+// (DO NOT EDIT: machine generated header) 'LibreDelphi.pas' rev: 37.00 (Windows)
+
+#ifndef LibreDelphiHPP
+#define LibreDelphiHPP
+
+#pragma delphiheader begin
+#pragma option push
+#if defined(__BORLANDC__) && !defined(__clang__)
+#pragma option -w-      // All warnings off
+#pragma option -Vx      // Zero-length empty class member 
+#endif
+#pragma pack(push,8)
+#include <System.hpp>
+#include <SysInit.hpp>
+#include <System.Net.URLClient.hpp>
+#include <System.Net.HttpClient.hpp>
+#include <System.Net.HttpClientComponent.hpp>
+#include <System.Net.Mime.hpp>
+#include <System.JSON.hpp>
+#include <System.Types.hpp>
+#include <System.UITypes.hpp>
+#include <System.Classes.hpp>
+#include <System.SysUtils.hpp>
+#include <System.Variants.hpp>
+#include <System.Generics.Collections.hpp>
+#include <System.Messaging.hpp>
+
+//-- user supplied -----------------------------------------------------------
+
+namespace Libredelphi
+{
+//-- forward type declarations -----------------------------------------------
+class DELPHICLASS TLibreTrans;
+//-- type declarations -------------------------------------------------------
+enum DECLSPEC_DENUM TLibreEndpointSite : unsigned char { lesLibreTranslateMain, lesLibreTranslateDE, lesArgosOpenTech, lesVernCC, lesFortyTwoIT, lesTerraPrint, lesSkitzen, lesZillyHuhn, lesFedilab };
+
+class PASCALIMPLEMENTATION TLibreTrans : public System::Classes::TComponent
+{
+	typedef System::Classes::TComponent inherited;
+	
+private:
+	System::UnicodeString FAPI;
+	TLibreEndpointSite FSite;
+	System::UnicodeString FEndpoint;
+	System::Net::Httpclientcomponent::TNetHTTPClient* FNet;
+	void __fastcall SetSite(const TLibreEndpointSite Value);
+	bool __fastcall SitOnline(const System::UnicodeString URL);
+	
+protected:
+	System::UnicodeString __fastcall DefaultApiKey();
+	
+public:
+	__fastcall virtual TLibreTrans(System::Classes::TComponent* AOwner);
+	__fastcall virtual ~TLibreTrans();
+	System::UnicodeString __fastcall CheckLng(const System::UnicodeString Text);
+	System::Classes::TStringList* __fastcall ListLng();
+	System::Classes::TStringList* __fastcall CodLng();
+	System::UnicodeString __fastcall Translate(const System::UnicodeString Text, const System::UnicodeString OrgLng, const System::UnicodeString DstLng);
+	System::UnicodeString __fastcall AutoTranslate(const System::UnicodeString Text, const System::UnicodeString DstLng);
+	System::UnicodeString __fastcall TranslateFile(const System::UnicodeString Orig, const System::UnicodeString Dest, const System::UnicodeString LngOrig, const System::UnicodeString LngDest);
+	void __fastcall AutoEndpoint();
+	__property System::UnicodeString Endpoint = {read=FEndpoint};
+	
+__published:
+	__property System::UnicodeString API = {read=FAPI, write=FAPI};
+	__property TLibreEndpointSite Site = {read=FSite, write=SetSite, default=0};
+};
+
+
+//-- var, const, procedure ---------------------------------------------------
+extern DELPHI_PACKAGE void __fastcall Register(void);
+}	/* namespace Libredelphi */
+#if !defined(DELPHIHEADER_NO_IMPLICIT_NAMESPACE_USE) && !defined(NO_USING_NAMESPACE_LIBREDELPHI)
+using namespace Libredelphi;
+#endif
+#pragma pack(pop)
+#pragma option pop
+
+#pragma delphiheader end.
+//-- end unit ----------------------------------------------------------------
+#endif	// LibreDelphiHPP
---
+++ Win64x/Release/LibreT2Delphi.hpp
@@ -0,0 +1,103 @@
+﻿// CodeGear C++Builder
+// Copyright (c) 1995, 2025 by Embarcadero Technologies, Inc.
+// All rights reserved
+
+// (DO NOT EDIT: machine generated header) 'LibreT2Delphi.dpk' rev: 37.00 (Windows)
+
+#ifndef LibreT2DelphiHPP
+#define LibreT2DelphiHPP
+
+#pragma delphiheader begin
+#pragma option push
+#if defined(__BORLANDC__) && !defined(__clang__)
+#pragma option -w-      // All warnings off
+#pragma option -Vx      // Zero-length empty class member 
+#endif
+#pragma pack(push,8)
+#include <System.hpp>	// (rtl)
+#include <SysInit.hpp>
+#include <LibreDelphi.hpp>
+#include <System.Types.hpp>	// (rtl)
+#include <System.UITypes.hpp>	// (rtl)
+#include <Winapi.Windows.PkgHelper.hpp>	// (rtl)
+#include <Winapi.Windows.hpp>	// (rtl)
+#include <Winapi.Messages.hpp>	// (rtl)
+#include <System.SysConst.hpp>	// (rtl)
+#include <Winapi.ImageHlp.hpp>	// (rtl)
+#include <Winapi.SHFolder.hpp>	// (rtl)
+#include <Winapi.PsAPI.hpp>	// (rtl)
+#include <System.RTLConsts.hpp>	// (rtl)
+#include <System.Character.hpp>	// (rtl)
+#include <System.Internal.ExcUtils.hpp>	// (rtl)
+#include <System.SysUtils.hpp>	// (rtl)
+#include <System.VarUtils.hpp>	// (rtl)
+#include <System.Variants.hpp>	// (rtl)
+#include <Winapi.ActiveX.hpp>	// (rtl)
+#include <System.TypInfo.hpp>	// (rtl)
+#include <System.Hash.hpp>	// (rtl)
+#include <System.Math.hpp>	// (rtl)
+#include <System.Generics.Defaults.hpp>	// (rtl)
+#include <System.Generics.Collections.hpp>	// (rtl)
+#include <System.TimeSpan.hpp>	// (rtl)
+#include <System.Diagnostics.hpp>	// (rtl)
+#include <System.SyncObjs.hpp>	// (rtl)
+#include <System.Rtti.hpp>	// (rtl)
+#include <System.Classes.hpp>	// (rtl)
+#include <Winapi.CommCtrl.hpp>	// (rtl)
+#include <Winapi.Qos.hpp>	// (rtl)
+#include <Winapi.Winsock2.hpp>	// (rtl)
+#include <Winapi.IpExport.hpp>	// (rtl)
+#include <Winapi.ShellAPI.hpp>	// (rtl)
+#include <Winapi.RegStr.hpp>	// (rtl)
+#include <Winapi.WinInet.hpp>	// (rtl)
+#include <Winapi.UrlMon.hpp>	// (rtl)
+#include <Winapi.ObjectArray.hpp>	// (rtl)
+#include <Winapi.StructuredQueryCondition.hpp>	// (rtl)
+#include <Winapi.PropSys.hpp>	// (rtl)
+#include <Winapi.MSXMLIntf.hpp>	// (rtl)
+#include <Winapi.ShlObj.hpp>	// (rtl)
+#include <Winapi.KnownFolders.hpp>	// (rtl)
+#include <System.Masks.hpp>	// (rtl)
+#include <System.StrUtils.hpp>	// (rtl)
+#include <System.DateUtils.hpp>	// (rtl)
+#include <System.IOUtils.hpp>	// (rtl)
+#include <System.IniFiles.hpp>	// (rtl)
+#include <System.Win.Registry.hpp>	// (rtl)
+#include <System.NetConsts.hpp>	// (rtl)
+#include <System.Net.Mime.hpp>	// (rtl)
+#include <System.Threading.hpp>	// (rtl)
+#include <System.NetEncoding.hpp>	// (rtl)
+#include <System.Net.URLClient.hpp>	// (rtl)
+#include <Winapi.WinHTTP.hpp>	// (rtl)
+#include <System.Net.HttpClient.Win.hpp>	// (rtl)
+#include <System.AnsiStrings.hpp>	// (rtl)
+#include <System.Win.Crtl.hpp>	// (rtl)
+#include <System.ZLib.hpp>	// (rtl)
+#include <System.Net.HttpClient.hpp>	// (rtl)
+#include <System.Net.HttpClientComponent.hpp>	// (rtl)
+#include <System.JSONConsts.hpp>	// (rtl)
+#include <System.JSON.hpp>	// (rtl)
+#include <System.Messaging.hpp>	// (rtl)
+// SO_SFX: 370
+// PRG_EXT: .bpl
+// BPI_DIR: C:\Users\Public\Documents\Embarcadero\Studio\37.0\Dcp\Win64x
+// OBJ_DIR: C:\Users\Public\Documents\Embarcadero\Studio\37.0\Dcp\Win64x
+// OBJ_EXT: .o
+
+//-- user supplied -----------------------------------------------------------
+
+namespace Libret2delphi
+{
+//-- forward type declarations -----------------------------------------------
+//-- type declarations -------------------------------------------------------
+//-- var, const, procedure ---------------------------------------------------
+}	/* namespace Libret2delphi */
+#if !defined(DELPHIHEADER_NO_IMPLICIT_NAMESPACE_USE) && !defined(NO_USING_NAMESPACE_LIBRET2DELPHI)
+using namespace Libret2delphi;
+#endif
+#pragma pack(pop)
+#pragma option pop
+
+#pragma delphiheader end.
+//-- end unit ----------------------------------------------------------------
+#endif	// LibreT2DelphiHPP
